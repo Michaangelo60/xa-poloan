2ae1575ce9f042ed5cf6dc53cde1dc4d67dcf80d
Michaangelo60 <155946504+Michaangelo60@users.noreply.github.com>
2026-01-03 21:34:04 +0100

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,8 @@ module.exports = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/xapobank',
   JWT_SECRET: process.env.JWT_SECRET || 'change-me',
-  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:8000'
+  CLIENT_URL: process.env.CLIENT_URL || 'https://xa-poloan.onrender.com'
   ,ADMIN_EMAIL: process.env.ADMIN_EMAIL || null
   ,ADMIN_ID: process.env.ADMIN_ID || null
 };
+
