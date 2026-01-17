6f82321a1be866fe52efc7863e35ec098d4fda27
Michaangelo60 <155946504+Michaangelo60@users.noreply.github.com>
2026-01-17 12:54:24 +0100

    Delete backend directory

--- backend/routes/identity.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const identityController = require('../controllers/identityController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/upload', authMiddleware, identityController.upload);
-
-module.exports = router;
--- backend/routes/support.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendSupport } = require('../controllers/supportController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-// Allow anonymous messages but also accept authenticated users
-router.post('/', authMiddleware, sendSupport);
-
-module.exports = router;
